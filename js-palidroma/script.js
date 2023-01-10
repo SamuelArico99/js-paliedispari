d41bf04bf80f46d39db99bf045f4a5c00fa6dbae
SamuelArico99 <samuel16699.sa@gmail.com>
2023-01-10 16:54:08 +0100

    palidroma

---
+++ js-palidroma/script.js
@@ -0,0 +1,22 @@
+let parola = prompt('inserisci una parola');
+let parolaInversa = invertiParola(parola);
+
+function invertiParola(invert){
+  let parolaInversa = '';
+
+  let i = invert.length - 1;
+
+  while (i >= 0) {
+    parolaInversa += invert[i];
+    i--;
+  }
+
+  return parolaInversa;
+}
+
+if(parola == parolaInversa){
+    alert('la parola è palindroma');
+  } else {
+    alert('la parola non è palindroma');
+}
+
